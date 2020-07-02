11d61c6ae27c8c9f24c43130991981e1baec6261
Varun <varunsridharan23@gmail.com>
2020-07-02 11:25:51 +0530

    Added `GITHUB_REPO_SLUG` ENV

--- app/app.php
+++ app/app.php
@@ -1,8 +1,29 @@
 <?php
+
+use Milo\Github\Api;
+use Milo\Github\OAuth\Token;
+
+set_time_limit( 0 );
 define( 'APP_PATH', __DIR__ . '/' );
 
 require_once APP_PATH . 'functions.php';
+require_once APP_PATH . 'vendor/autoload.php';
 
 if ( empty( get_env( 'GITHUB_TOKEN' ) ) ) {
 	die( '::error:: 🛑 GITHUB_TOKEN ENV is not set !' );
+}
+
+$token  = get_env( 'GITHUB_TOKEN' );
+$repo   = get_env( 'GITHUB_REPOSITORY' );
+$gh_api = new Api();
+
+$gh_api->setToken( new Token( $token ) );
+$data = $gh_api->decode( $gh_api->get( 'repos/' . $repo ) );
+
+if ( empty( $data ) ) {
+	die( '::error:: 🛑  Unable To Fetch Data From Github Api' );
+}
+
+if ( isset( $data['name'] ) ) {
+	set_action_env_not_exists( 'GITHUB_REPO_SLUG', $data['name'] );
 }
