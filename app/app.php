7bd2ef024c0c8b8181a613221e159ce73fe153f6
Varun <varunsridharan23@gmail.com>
2020-07-02 11:57:01 +0530

    Added Quite Few ENV Vars

--- app/app.php
+++ app/app.php
@@ -21,8 +21,53 @@ $data   = $gh_api->decode( $gh_api->get( 'repos/' . $repo ) );
 if ( empty( $data ) ) {
 	die( '::error:: 🛑  Unable To Fetch Data From Github Api' );
 }
-var_dump( $data->name );
-var_dump( $data );
+
 if ( isset( $data->name ) ) {
-	set_action_env_not_exists( 'GITHUB_REPO_SLUG', $data->name );
+	set_action_env_not_exists( 'REPOSITORY_SLUG', $data->name );
+	set_action_env_not_exists( 'REPOSITORY_NAME', ucwords( str_replace( '-', ' ', $data->name ) ) );
+}
+
+if ( isset( $data->full_name ) ) {
+	set_action_env_not_exists( 'REPOSITORY_FULL_NAME', $data->full_name );
+}
+
+if ( isset( $data->private ) ) {
+	set_action_env_not_exists( 'REPOSITORY_IS_PRIVATE', ( isset( $data->private ) && true === $data->private ) ? 'yes' : 'no' );
+}
+
+if ( isset( $data->fork ) ) {
+	set_action_env_not_exists( 'REPOSITORY_IS_FORK', ( isset( $data->fork ) && true === $data->fork ) ? 'yes' : 'no' );
+}
+
+if ( isset( $data->html_url ) ) {
+	set_action_env_not_exists( 'REPOSITORY_GITHUB_URL', $data->html_url );
+}
+
+if ( isset( $data->homepage ) ) {
+	set_action_env_not_exists( 'REPOSITORY_HOMEPAGE_URL', $data->homepage );
+}
+
+if ( isset( $data->description ) ) {
+	set_action_env_not_exists( 'REPOSITORY_DESCRIPTION', $data->description );
+}
+
+if ( isset( $data->created_at ) ) {
+	set_action_env_not_exists( 'REPOSITORY_CREATED_AT', $data->created_at );
+}
+
+if ( isset( $data->updated_at ) ) {
+	set_action_env_not_exists( 'REPOSITORY_UPDATED_AT', $data->updated_at );
+}
+
+if ( isset( $data->pushed_at ) ) {
+	set_action_env_not_exists( 'REPOSITORY_PUSHED_AT', $data->pushed_at );
+}
+
+if ( isset( $data->default_branch ) ) {
+	set_action_env_not_exists( 'REPOSITORY_DEFAULT_BRANCH', $data->default_branch );
+}
+
+if ( ! empty( get_env( 'GITHUB_SHA', false ) ) ) {
+	$sha_short = substr( get_env( 'GITHUB_SHA', false ), 0, 8 );
+	set_action_env_not_exists( 'SHA_SHORT', $sha_short );
 }
