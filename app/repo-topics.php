c3256cd37cbc38674e7306ea124f649ff19aaf14
Varun <varunsridharan23@gmail.com>
2020-07-03 09:21:28 +0530

    Updated

--- app/repo-topics.php
+++ app/repo-topics.php
@@ -6,11 +6,12 @@ if ( isset( $data->topics ) ) {
 }
 
 if ( empty( $topics ) ) {
-	$topics = $gh_api->decode( $gh_api->get( 'repos/varunsridharan/tag/topics', [], [ 'Accept' => 'application/vnd.github.mercy-preview+json' ] ) );
-	if ( isset( $topics->names ) && ! empty( $topics->names ) ) {
-		$topics = $topics->names;
-	} else {
-		$topics = false;
+	try {
+		$topics = $gh_api->decode( $gh_api->get( 'repos/varunsridharan/tag/topics', [], [] ) );
+		$topics = ( isset( $topics->names ) && ! empty( $topics->names ) ) ? $topics->names : false;
+	} catch ( \Milo\Github\UnexpectedResponseException $exception ) {
+		$topics = $gh_api->decode( $gh_api->get( 'repos/varunsridharan/tag/topics', [], [ 'Accept' => 'application/vnd.github.mercy-preview+json' ] ) );
+		$topics = ( isset( $topics->names ) && ! empty( $topics->names ) ) ? $topics->names : false;
 	}
 }
 
