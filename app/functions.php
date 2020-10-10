c02b9841f014a5c831e9259af76bd8e234c97ce8
Varun <varunsridharan23@gmail.com>
2020-10-10 10:10:47 +0530

    Trying Shell Exec

--- app/functions.php
+++ app/functions.php
@@ -8,7 +8,8 @@
  * @return true
  */
 function set_action_env( $key, $value ) {
-	_echo( 'echo "' . $key . '=' . $value . '" >> $GITHUB_ENV' );
+	shell_exec( 'echo "' . $key . '=' . $value . '" >> $GITHUB_ENV' );
+	//echo( 'echo "' . $key . '=' . $value . '" >> $GITHUB_ENV' );
 	$_ENV[ $key ] = $value;
 	return true;
 }
