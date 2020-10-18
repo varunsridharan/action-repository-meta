8676afc4a11b07599d733d27da8b2c0088d17c50
Varun <varunsridharan23@gmail.com>
2020-10-18 09:27:17 +0530

    Added print_r

--- app/app.php
+++ app/app.php
@@ -24,6 +24,8 @@ $gh_api = new Api();
 $gh_api->setToken( new Token( $token ) );
 $data = $gh_api->decode( $gh_api->get( 'repos/' . $repo ) );
 
+print_r($data);
+
 if ( empty( $data ) ) {
 	_error( 'Unable To Fetch Data From Github Api' );
 }
