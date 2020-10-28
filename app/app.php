0162a457049bececb7a54369143f2c451a0acf47
Varun <varunsridharan23@gmail.com>
2020-10-28 19:17:42 +0530

    Updated

--- app/app.php
+++ app/app.php
@@ -1,7 +1,8 @@
 <?php
-global $data, $github_api, $repo;
+global $data, $github_api;
 
 set_time_limit( 0 );
+
 error_reporting( E_ALL );
 
 define( 'APP_PATH', __DIR__ . '/' );
@@ -10,25 +11,14 @@ require_once '/gh-toolkit/php.php';
 require_once '/gh-toolkit/gh-api.php';
 require_once APP_PATH . 'functions.php';
 
-define( 'REQUEST_REPOSITORY', gh_env( 'GITHUB_REPOSITORY' ) );
+
+define( 'REQUEST_REPOSITORY', gh_input( 'REPOSITORY', gh_env( 'GITHUB_REPOSITORY' ) ) );
 
 $data = $github_api->decode( $github_api->get( 'repos/' . REQUEST_REPOSITORY ) );
 
 if ( empty( $data ) ) {
-	gh_log_error( 'Unable To Fetch Data From Github Api' );
+	gh_log_error( 'Unable To Fetch Repository Data via API For : ' . REQUEST_REPOSITORY );
 }
-
-/*gh_log_group_start( 'GH API VAR' );
-print_r( $data );
-gh_log_group_end();
-
-gh_log_group_start( '$_ENV VAR' );
-print_r( $_ENV );
-gh_log_group_end();
-
-gh_log_group_start( 'GITHUB_EVENT_PATH VAR' );
-print_r( json_decode( file_get_contents( $_ENV['GITHUB_EVENT_PATH'] ), true ) );
-gh_log_group_end();*/
 
 require_once APP_PATH . 'includes/basic.php';
 require_once APP_PATH . 'includes/community.php';
